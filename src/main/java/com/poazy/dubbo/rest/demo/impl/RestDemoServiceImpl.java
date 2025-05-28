1de04a5308d5bd80d7dc0e54b2755bfa4da2995e
duanbo <boazy@126.com>
2025-05-28 15:19:31 +0800

    Add sample documentation description

--- src/main/java/com/poazy/dubbo/rest/demo/impl/RestDemoServiceImpl.java
+++ src/main/java/com/poazy/dubbo/rest/demo/impl/RestDemoServiceImpl.java
@@ -5,13 +5,14 @@ import com.poazy.dubbo.rest.demo.api.entity.DrdRQ;
 import com.poazy.dubbo.rest.demo.api.entity.DrdRS;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
 @DubboService
 public class RestDemoServiceImpl implements RestDemoService {
 
     @Override
-    public DrdRS<Long> queryPrice(DrdRQ<Long> reqArgs) {
+    public DrdRS<Long> queryPrice(@RequestBody  DrdRQ<Long> reqArgs) {
         DrdRS<Long> rs = new DrdRS<>();
         rs.setData(reqArgs.getData());
         rs.setCode(200);
